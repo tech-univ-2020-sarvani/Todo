056def51e4660139596403c24d0952107cfb6455
sarvanideekshitula <sarvanimini@gmail.com>
2020-01-27 16:45:23 +0530

    Changes made to the server.js file

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes/todoRoutes.js');
 const Joi = require('@hapi/joi');
-const server = Hapi.Server({
-	host: 'localhost',
-	port: 8080
-});
-server.route(routes);
-	
-const start = async () => {
-	
-	await server.validator(Joi);
-	await server.start();
+
+const start = () => {
+	const server = Hapi.Server({
+		host: 'localhost',
+		port: 8080
+	});
+	server.route(routes);
+	server.validator(Joi);
 	console.log(`Server running at: ${server.info.uri}`);
 	return server;
 };
-// start();
-module.exports = server;
+
+module.exports = start;
